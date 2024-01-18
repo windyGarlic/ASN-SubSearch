f3ce36f3f1a201ea0b6eeda32df8992fea58bfa9
Jordan Swebeck <111098407+windyGarlic@users.noreply.github.com>
2024-01-18 18:47:03 +1100

    Update asnSubSearch.sh

--- asnSubSearch.sh
+++ asnSubSearch.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-# subSeachASN 
+while getopts ":d:a:h" opt; do
+  case $opt in
+    d)
+      DOMAIN="$OPTARG"
+      ;;
+    a)
+      ASN="$OPTARG"
+      ;;
+    h)
+      echo "Usage: $0 -d <domain> -a <ASN>"
+      echo "Options:"
+      echo "  -d  Domain name (required)"
+      echo "  -a  ASN (optional but preferred)"
+      echo "  -h  Display this help menu"
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
-# TO DO
-# 	Create asn.masterlist collecting: - company -  asn - ipRange - ip/CIDR - database text file 
-
-# WHAT IT CAN DO
-# 	Creates file: <company>.ipRange - a list all ip ranges owned by companies ASN
-# 	Creates file: <company>.ipList - unpacked list of all IP in ASN IP allranges
-# 	Creates file: <company>.newSubs - A list of the newly found subdomains
-#	Runs every IP through hakip2host & dnsx to resolve and validate endpoints
-
-
-# [+]  Read these vaules in through args or change var names to work in existing script
-DOMAIN="example.com"
-ASN="00000" 				# ASN Optional but prefered!
+# Validate required options
+if [ -z "$DOMAIN" ]; then
+  echo "Error: Domain (-d) is required. Use -h for help."
+  exit 1
+fi
 
 
 # Get asn data list 	(Uncomment if you dont already have the ASN data)
@@ -74,6 +91,7 @@ if [[ -s $COMPANY.ipRange ]]; then
         exit 1
     fi
 
+
     if [[ -s $COMPANY.ipRange ]]; then
     echo '[+] Found IP ranges'
     while read line; do
@@ -93,10 +111,24 @@ if [[ -s $COMPANY.ipRange ]]; then
     fi
 fi
 
+# Creates list of all IP's contained in range
+for line in $(cat $COMPANY.ipRange); do
+    start_ip=$(echo $line | awk -F " - " '{print $1}')
+    end_ip=$(echo $line | awk -F " - " '{print $2}')
+    start_int=$(ip_to_int "$start_ip")
+    end_int=$(ip_to_int "$end_ip")
+
+    for ((int = start_int; int <= end_int; int++)); do
+        current_ip=$(int_to_ip "$int")
+        echo "$current_ip" >> $COMPANY.ipList
+    done
+done 
+
 cat $COMPANY.ipList | hakip2host | sort | uniq > $DOMAIN.newSubs.tmp 
 cat $DOMAIN.newSubs.tmp | awk -F " " '{print$3}' | sort | uniq > $DOMAIN.newSubs.sub 
 cat $DOMAIN.newSubs.sub | grep -v "*" | dnsx -silent > $COMPANY.newSubs
 
-#rm $DOMAIN.newSubs.sub 
+rm $DOMAIN.newSubs.sub 
 rm $DOMAIN.newSubs.tmp 
 echo '[+] new subdomains found and stored as ' $COMPANY'.newSubs' 
+cat $COMPANY.newSubs
